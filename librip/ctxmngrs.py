635326a120c67b217ad627c3de7981cd5be7691a
Кучеренко Михаил <m.kucherenko@corp.mail.ru>
2018-12-07 01:59:19 +0300

    Solved ex_5

--- librip/ctxmngrs.py
+++ librip/ctxmngrs.py
@@ -6,3 +6,19 @@
 #   sleep(5.5)
 #
 # После завершения блока должно вывестись в консоль примерно 5.5
+
+from datetime import datetime
+
+
+class timer(object):
+
+    def __init__(self):
+        self.start = None
+
+    def __enter__(self):
+        self.start = datetime.now()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print('Elapsed time:', (datetime.now() - self.start).total_seconds())
+        if exc_val:
+            raise
